2a23926531f3a651f5715747945e4055693bc143
ikamil1998 <i.kamil@bdassmart.com>
2024-01-20 13:48:41 +0200

    migration

--- migrations/20240120105338-add-id-column-to-ach-dep.js
+++ migrations/20240120105338-add-id-column-to-ach-dep.js
@@ -3,12 +3,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('AchievementDepartment', 'id',{
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER,
-    },);
+    // await queryInterface.addColumn('AchievementDepartment', 'id',{
+    //   allowNull: false,
+    //   autoIncrement: true,
+    //   primaryKey: true,
+    //   type: Sequelize.INTEGER,
+    // },);
   },
 
   down: async (queryInterface, Sequelize) => {
---
+++ migrations/20240120114556-remove_ibk_achievment_dep.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  //  Remove the existing foreign key constraint images_ibfk_1
+    await queryInterface.removeConstraint('AchievementDepartment', 'AchievementDepartment_ibfk_1');
+
+    // Add a new foreign key constraint referencing the 'Achievements' table
+    await queryInterface.addConstraint('AchievementDepartment', {
+      fields: ['achievmentId'],
+      type: 'foreign key',
+      name: 'AchievementDepartment_ibfk_9', // Choose a new name for the foreign key constraint
+      references: {
+        table: 'Achievements', // Change to the actual name of your Achievements table
+        field: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('AchievementDepartment', 'AchievementDepartment_ibfk_9');
+
+    // Recreate the original foreign key constraint referencing the 'achievments' table
+    await queryInterface.addConstraint('AchievementDepartment', {
+      fields: ['achievementId'],
+      type: 'foreign key',
+      name: 'AchievementDepartment_ibfk_1',
+      references: {
+        table: 'achievments', // Change to the actual name of your original Achievements table
+        field: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+  },
+};
